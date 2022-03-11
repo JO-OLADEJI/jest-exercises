856bae0d1cd646a6d66cab1228e3f2bd2c968a4e
Joshua Oladeji <dejijolaoluwa@gmail.com>
2022-03-11 01:13:20 +0100

    initial commit

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+// Write a function that takes in a number and returns one of the following:
+
+// If the number is divisible by 3, return "Fizz".
+// If the number is divisible by 5, return "Buzz".
+// If the number is divisible by both 3 and 5, return "FizzBuzz".
+// Otherwise, just return the number that was passed into the function.
+
+const fizzBuzz = (number) => {
+
+}
+
+module.exports = fizzBuzz;
---
+++ tests/fizzBuzz.test.js
@@ -0,0 +1,15 @@
+describe('fizzbuzz', () => {
+
+  it('should return "FizzBuzz" when divisible by 3 and 5', () => {
+    // TODO
+  });
+
+  it('should return "Fizz" if number is divisible by 3', () => {
+    // TODO
+  });
+
+  it('should return "Buzz" if number is divisible by 5', () => {
+    // TODO
+  });
+
+});
